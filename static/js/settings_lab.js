6a22bc56baca26d042bf36bf439963052c7337c9
Oskar Olofsson <oskar.olofsson@hotmail.com>
2018-02-16 23:58:04 +0100

    refactor: changed lines in settings_lab and subs

--- static/js/settings_lab.js
+++ static/js/settings_lab.js
@@ -17,13 +17,19 @@ exports.set_up = function () {
                 url: '/json/settings/ui',
                 data: labs_updates,
                 success: function () {
-                    var message = i18n.t("Updated settings!  You will need to reload for these changes to take effect.", page_params);
+                    var message = i18n.t(
+                      "Updated settings!"
+                      + " You will need to reload for these changes to take effect.",
+                      page_params);
                     var ui_settings_status = $('#ui-settings-status').expectOne();
 
                     ui_report.success(message, ui_settings_status);
                 },
                 error: function (xhr) {
-                    ui_report.error(i18n.t("Error changing settings"), xhr, $('#ui-settings-status').expectOne());
+                    ui_report.error(
+                      i18n.t("Error changing settings"),
+                      xhr,
+                      $('#ui-settings-status').expectOne());
                 },
             });
         });
